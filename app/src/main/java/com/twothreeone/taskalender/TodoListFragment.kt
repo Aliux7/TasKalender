91f62b0c79ebdbb822ada8cd09d1bb651e109fe1
Trinity <trinity.loren@binus.ac.id>
2023-10-12 11:23:37 +0700

    task horizontal scroll menu

--- app/src/main/java/com/twothreeone/taskalender/TodoListFragment.kt
+++ app/src/main/java/com/twothreeone/taskalender/TodoListFragment.kt
@@ -1,10 +1,16 @@
 package com.twothreeone.taskalender
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView
+import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView.OnHSMenuClickListener
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -12,6 +18,9 @@ private const val ARG_PARAM2 = "param2"
 class TodoListFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var horizontalScrollView: HorizontalScrollMenuView
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +30,32 @@ class TodoListFragment : Fragment() {
         }
     }
 
+    private fun initMenu() {
+        if (::horizontalScrollView.isInitialized) {
+            horizontalScrollView.addItem("Default", 0)
+            horizontalScrollView.addItem("Home", 0)
+            horizontalScrollView.addItem("Work", 0)
+            horizontalScrollView.addItem("Add", 0)
+
+            horizontalScrollView.setOnHSMenuClickListener(OnHSMenuClickListener { menuItem, position ->
+                textView.text = menuItem.text
+            })
+        } else {
+            Log.e("YourTag", "horizontalScrollView is not initialized, cannot add items")
+        }
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_todo_list, container, false)
+        horizontalScrollView = rootView.findViewById(R.id.taskMenu)
+        textView = rootView.findViewById((R.id.textView))
+        initMenu()
+
+        // You can now access horizontalScrollView
+        return rootView
     }
 }
