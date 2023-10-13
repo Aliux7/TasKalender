6a3f73da24afd0ad54c06a69d2dd15de86610ed3
Trinity <trinity.loren@binus.ac.id>
2023-10-13 13:08:13 +0700

    Edit profile ui

---
+++ app/src/main/java/edu/bluejack23_1/taskalender/EditProfileActivity.kt
@@ -0,0 +1,14 @@
+package edu.bluejack23_1.taskalender
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.twothreeone.taskalender.R
+
+class EditProfileActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+
+}
