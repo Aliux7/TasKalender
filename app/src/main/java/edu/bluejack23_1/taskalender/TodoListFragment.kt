519c363a9e9515fd253eccc3048712cd0d34821f
Trinity <trinity.loren@binus.ac.id>
2023-10-13 09:01:31 +0700

    task on progress

--- app/src/main/java/edu/bluejack23_1/taskalender/TodoListFragment.kt
+++ app/src/main/java/edu/bluejack23_1/taskalender/TodoListFragment.kt
@@ -7,13 +7,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView
 import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView.OnHSMenuClickListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.twothreeone.taskalender.R
 
 
@@ -27,6 +33,8 @@ class TodoListFragment : Fragment() {
     private lateinit var horizontalScrollView: HorizontalScrollMenuView
     private lateinit var textView: TextView
     private lateinit var addTaskBtn: Button
+    private var isPopupVisible = false
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +46,6 @@ class TodoListFragment : Fragment() {
 
     private fun initMenu() {
         if (::horizontalScrollView.isInitialized) {
-            horizontalScrollView.addItem("Default", 0)
             horizontalScrollView.addItem("Home", 0)
             horizontalScrollView.addItem("Work", 0)
             horizontalScrollView.addItem("Add", 0)
@@ -54,8 +61,23 @@ class TodoListFragment : Fragment() {
     private fun showAddTaskPopup(){
         val dialog = Dialog(requireContext())
         dialog.setContentView(R.layout.layout_add_task_dialog)
+        val window = dialog.window
+        val params = window?.attributes
+        params?.width = ViewGroup.LayoutParams.MATCH_PARENT
+        window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+// Set the height of the dialog to wrap content
+        params?.height = ViewGroup.LayoutParams.WRAP_CONTENT
+
+        window?.attributes = params
+        window?.attributes = params
+
+        dialog.window?.setBackgroundDrawableResource(R.color.background_color)
 
         val taskEditText = dialog.findViewById<EditText>(R.id.addTaskTxt)
+        taskEditText.requestFocus()
+        val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT)
+
         val submitTaskButton = dialog.findViewById<Button>(R.id.submitTaskBtn)
 
         // Show the keyboard
@@ -67,12 +89,40 @@ class TodoListFragment : Fragment() {
 
         submitTaskButton.setOnClickListener {
             val taskText = taskEditText.text.toString()
-            // Handle the task text (e.g., save it, update UI, etc.)
-
             dialog.dismiss()
+            addDataToFirestore(taskEditText)
         }
 
+        dialog.setOnDismissListener {
+            isPopupVisible = false
+            showAddTaskButton()
+        }
+        isPopupVisible = true
+
         dialog.show()
+    }
+
+    private fun hideAddTaskButton() {
+        // Apply the fade-out animation to the button
+        val fadeOut = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out)
+        fadeOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Hide the button when the fade-out animation ends
+                addTaskBtn.visibility = View.INVISIBLE
+            }
+        })
+        addTaskBtn.startAnimation(fadeOut)
+    }
+
+    private fun showAddTaskButton() {
+        // Apply the fade-in animation to the button
+        val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+        addTaskBtn.startAnimation(fadeIn)
+
+        addTaskBtn.visibility = View.VISIBLE
     }
 
 
@@ -86,10 +136,37 @@ class TodoListFragment : Fragment() {
         addTaskBtn = rootView.findViewById(R.id.addTaskBtn)
         initMenu()
         addTaskBtn.setOnClickListener {
-            showAddTaskPopup()
+            if (!isPopupVisible) {
+                hideAddTaskButton()
+                showAddTaskPopup()
+            }
         }
 
         // You can now access horizontalScrollView
         return rootView
+    }
+
+    private fun addDataToFirestore(taskText: EditText) {
+        val taskTextValue = taskText.text.toString()
+
+        // Define the data to be added
+        val data = hashMapOf(
+            "name" to taskTextValue
+        )
+
+        val collectionReference = db.collection("tasks")
+        collectionReference
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                val autoGeneratedId = documentReference.id
+                showToast("Task added")
+            }
+            .addOnFailureListener { e ->
+                showToast("Failed to add task: ${e.message}")
+            }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
